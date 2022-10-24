1969c873e2b6040bf9d601ced4d3f280ce6d10d7
Arif khan <barfikhan78621@gmail.com>
2022-10-24 11:10:18 +0530

    First Commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { NumberSymbol } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  Designation:string;
+  Username:string;
+  NoOfTeamMembers:number;
+  TotalCostOfAllProjects:number;
+  PendingTasks:number;
+  UpComingProjects:number;
+  ProjectCost:number;
+  CurrentExpenditure:number;
+  AvailableFunds:number;
+
+
+  ngOnInit(): void {
+    this.Designation="Team Leader";
+    this.Username="John Smith";
+    this.NoOfTeamMembers=67;
+    this.TotalCostOfAllProjects=240;
+    this.PendingTasks=15;
+    this.UpComingProjects=2;
+    this.ProjectCost=2113507;
+    this.CurrentExpenditure=96788;
+    this.AvailableFunds=52536;
+  }
+
+}
